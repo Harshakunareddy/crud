6fed61f98dab19804dc9c638547abba309592194
Harshakunareddy <kunareddyharshareddy@gmail.com>
2025-10-26 20:22:18 +0530

    first-commit

--- src/config/db.js
+++ src/config/db.js
@@ -9,6 +9,8 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
+    logging: false,
+
     dialectOptions: {
       ssl: {
         require: true,
